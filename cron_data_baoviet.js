94c3fb72c025d83617553c1dee32225f64367015
Thinh Nguyen <thinhnguyen.teamx@gmail.com>
2020-10-09 15:22:51 +0700

    update combine function

--- cron_data_baoviet.js
+++ cron_data_baoviet.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const moment = require('moment');
+const dateFormat = require('dateformat');
 
 async function getDataFromAPI() {
   try {
@@ -13,21 +13,44 @@ async function getDataFromAPI() {
 function analystData(dataStocks) {
   console.log(dataStocks.length)
   return dataStocks.map(stock => {
-    // const formatDate = typeof (stock.TD) === 'string' ? moment(stock.TD).format("YYYY-MM-DD") : ''
-    // const convertedDate = formatDate && stock.FT ? `${formatDate} ${stock.FT}.000` : ''
+    var time = dateFormat(new Date(), "yyyy-mm-dd " + stock.FT);
     return {
-      time: stock.FT,
+      time,
       priceMatch: Number(stock.FMP) / 1000,
-      qttyMatch: stock.FV,
-      lenh: stock.LC ? stock.LC : ''
+      qttyMatch: Number(stock.FV),
+      lenh: stock.LC && stock.LC === 'S' ? 0 : 1
     }
   })
 }
 
+function combineSameData(listStocks) {
+  let data = {}
+  let arrData = []
+  listStocks.forEach(stock => {
+    let key = `${stock.FT}-${stock.TD}-${stock.FMP}-${stock.LC}`
+    if (data[key]) {
+      data[key] = {
+        ...data[key],
+        FV: Number(data[key].FV) + Number(stock.FV)
+      }
+    } else {
+      data[key] = stock
+    }
+  });
+  Object.keys(data).forEach(item => {
+    arrData.push(data[item])
+  })
+  return arrData
+}
+
 const cronData = async () => {
   const data = await getDataFromAPI()
-  const convertedData = await analystData(data)
-  console.log('convertedData', convertedData)
+  console.log('data raw', data.length)
+  const combinedData = await combineSameData(data)
+  console.log('combinedData', combinedData.length)
+  // const convertedData = await analystData(combinedData)
+
+  // console.log('combineData', combinedData)
 }
 
 cronData()
--- index_alltime.js
+++ index_alltime.js
@@ -1,5 +1,6 @@
 ﻿const axios = require('axios');
 const dateFormat = require('dateformat');
+const moment = require('moment');
 
 const sql = require('mssql');
 var config = {
@@ -16,24 +17,16 @@ var config = {
 }
 
 
+
 var insertStock = function (symdayid, symbol, stock) {
-    var time = dateFormat(new Date(), "yyyy-mm-dd " + stock.time);
+    var momentDate = moment(stock.tdate, 'DD.MM.YYYY');
+    var time = dateFormat(new Date(momentDate), "yyyy-mm-dd " + stock.time);
     var price = stock.priceMatch;
     var volume = parseInt(stock.qttyMatch);
     var lenh = stock.lenh;
     var insert_values_str = "'" + time + "'" + "," + price + "," + volume + "," + symdayid + "," + lenh;
     var request = new sql.Request();
-    request.query("insert into DetailDaily_1 ( dealtime, price, volume, symbolid, lenh )  values (" + insert_values_str + ")", function (err, results) {
-        if (err) console.log(err)
-    });
-}
-
-var updateStock = function (resultStock, existedRecord) {
-    var existedVolume = Number(existedRecord.volume);
-    var bonusVolume = Number(resultStock.qttyMatch);
-    var newVolume = bonusVolume + existedVolume;
-    var request = new sql.Request();
-    request.query(`update DetailDaily_1 set volume = ${newVolume} where id = ${existedRecord.id}`, function (err, results) {
+    request.query("insert into DetailDaily ( dealtime, price, volume, symbolid, lenh )  values (" + insert_values_str + ")", function (err, results) {
         if (err) console.log(err)
     });
 }
@@ -41,18 +34,17 @@ var updateStock = function (resultStock, existedRecord) {
 var loopStock = function (symbolid, symbol, resultStocks) {
     while (resultStocks && resultStocks.length > 0) {
         var resultStock = resultStocks.shift();
-        var time = dateFormat(new Date(), "yyyy-mm-dd " + resultStock.time);
+        //        var time = dateFormat(new Date(), "yyyy-mm-dd " + resultStock.time);
+        var momentDate = moment(resultStock.tdate, 'DD.MM.YYYY');
+        var time = dateFormat(new Date(momentDate), "yyyy-mm-dd " + resultStock.time);
+
         var request = new sql.Request();
-        var stringSQL = "select * from Detaildaily_1 where (dealtime = '" + time + "' AND  symbolid = " + symbolid + " AND  lenh = " + resultStock.lenh + ") ";
+        var stringSQL = "select * from DetailDaily where (dealtime = '" + time + "' AND  symbolid = " + symbolid + " AND  lenh = " + resultStock.lenh + " AND price = " + resultStock.priceMatch + ") ";
         request.query(stringSQL,
             function (err, results) {
                 var count = results && results.recordset && results.recordset.length ? results.recordset : [];
                 if (count.length === 0) {
                     insertStock(symbolid, symbol, resultStock);
-                    loopStock(symbolid, symbol, resultStocks);
-                } else {
-                    const existedRecord = results.recordset[0]
-                    updateStock(resultStock, existedRecord);
                     loopStock(symbolid, symbol, resultStocks);
                 }
             });
@@ -60,10 +52,31 @@ var loopStock = function (symbolid, symbol, resultStocks) {
     }
 }
 
-var fecthData = async function (symdayid, symbol, loopIndex) {
-    const data = await getDataFromAPI()
-    const convertedData = await analystData(data)
-    loopStock(symdayid, symbol, convertedData);
+function combineSameData(listStocks) {
+    let data = {}
+    let arrData = []
+    listStocks.forEach(stock => {
+        let key = `${stock.FT}-${stock.TD}-${stock.FMP}-${stock.LC}`
+        if (data[key]) {
+            data[key] = {
+                ...data[key],
+                FV: Number(data[key].FV) + Number(stock.FV)
+            }
+        } else {
+            data[key] = stock
+        }
+    });
+    Object.keys(data).forEach(item => {
+        arrData.push(data[item])
+    })
+    return arrData
+}
+
+var fecthData = async function (symbolid, symbol, loopIndex) {
+    const data = await getDataFromAPI(symbol)
+    const combineData = await combineSameData(data)
+    const convertedData = await analystData(combineData)
+    loopStock(symbolid, symbol, convertedData);
 }
 
 //Connect DB
@@ -71,51 +84,28 @@ var conn = sql.connect(config, function (err) {
     if (err) console.log(err);
     else {
         console.log('Conected');
-        // Đặt giờ chạy ở đây
-        var today = new Date();
-        var TimeA = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 9, 0, 1);
-        var TimeB = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 11, 31, 1);
-        var TimeC = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 13, 0, 0);
-        var TimeD = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 15, 3, 3);
 
         //Tao gia tran, san, TC
         var request = new sql.Request();
         setInterval(function () {
-            today = new Date();
-            if (today < TimeA) {
-                // chua den gio chay
-                console.log('Rinh-Vo-Chen-Luon');
-            }
-            else if (today > TimeB && today < TimeC) {
-                // nghi trua
-                console.log('Nghi trua');
-            }
-            else if (today > TimeD) {
-                // Het gio
-                console.log('Het gio');
-            }
-            else {
-                //Update price volum
-                request.query("select id, symbol, active from Symbols where (id = 1)", function (err, results) {
-                    if (err) console.log(err)
-                    var symbols = results && results.recordset && results.recordset.length ? results.recordset : [];
+            //Update price volum
+            request.query("select id, symbol, active from Symbols where (active = 1)", function (err, results) {
+                if (err) console.log(err)
+                var symbols = results && results.recordset && results.recordset.length ? results.recordset : [];
 
-                    symbols.forEach(function (symbol, index) {
-                        fecthData(symbol.id, symbol.symbol, index);
-                    });
-                    console.log('\x1b[32m%s\x1b[32m', "Update all completed: " + dateFormat(new Date(), "dd-mm-yyyy h:MM:ss"));
+                symbols.forEach(function (symbol, index) {
+                    fecthData(symbol.id, symbol.symbol, index);
                 });
-            }
+                console.log('\x1b[32m%s\x1b[32m', "Update all completed: " + dateFormat(new Date(), "dd-mm-yyyy h:MM:ss"));
+            });
         }, 5000);
     }
 
 });
 
-
-//
-async function getDataFromAPI() {
+async function getDataFromAPI(symbol) {
     try {
-        const res = await axios.get('https://online.bvsc.com.vn/datafeed/translogsnaps/BID')
+        const res = await axios.get('https://online.bvsc.com.vn/datafeed/translogsnaps/' + symbol.toUpperCase())
         return res.data.d
     } catch (error) {
         console.log(error)
@@ -129,7 +119,8 @@ function analystData(dataStocks) {
                 time: stock.FT,
                 priceMatch: Number(stock.FMP) / 1000,
                 qttyMatch: stock.FV,
-                lenh: stock.LC && stock.LC === 'S' ? 0 : 1
+                lenh: stock.LC && stock.LC === 'S' ? 0 : 1,
+                tdate: stock.TD
             }
         })
     } return []
